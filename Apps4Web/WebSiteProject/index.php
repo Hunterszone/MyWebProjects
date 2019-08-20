b26b868772d6192ec789422673d78956043434c3
hunterszone <k.drenski91@gmail.com>
2019-08-20 20:17:48 +0300

    images refactoring

--- Apps4Web/WebSiteProject/index.php
+++ Apps4Web/WebSiteProject/index.php
@@ -750,12 +750,12 @@
       <tr>
          <td>
             <div id="milkyway">
-               <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" height="200" width="auto">
+               <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" height="200" width="auto">
                   <param name="wmode" value="transparent">
-                  <param name="movie" value="http://parallelreality-bg.com/templates/reality/swf/milkyway.swf">
+                  <param name="movie" value="http://parallelreality-bg.com/templates/reality/images/milkyway2.png">
                   <param name="quality" value="best">
                   <param name="play" value="true">
-                  <embed height="91" pluginspage="http://www.macromedia.com/go/getflashplayer" src="http://parallelreality-bg.com/templates/reality/swf/milkyway.swf" type="application/x-shockwave-flash" bgcolor="#000000" width="115" quality="best" play="true" wmode="transparent">
+                  <embed height="91" src="http://parallelreality-bg.com/templates/reality/images/milkyway2.png" bgcolor="#000000" width="115" quality="best" play="true" wmode="transparent">
                </object>
             </div>
          </td>
