c47a87f4c080349501c060c25857d5a6479c62f6
Konstantin.Drenski <Konstantin.Drenski@LTBPSOF546.bpsf.bulpros.com>
2019-12-29 15:09:14 +0200

    component scan added

--- Apps4Web/TaskScheduler/src/main/java/com/drenski/scheduleapp/init/TaskScheduler.java
+++ Apps4Web/TaskScheduler/src/main/java/com/drenski/scheduleapp/init/TaskScheduler.java
@@ -3,9 +3,10 @@ package com.drenski.scheduleapp.init;
 import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.drenski.scheduleapp")
+@ComponentScan("com.drenski.scheduleapp.*")
 public class TaskScheduler {
 
 	public static void main(String[] args) {
