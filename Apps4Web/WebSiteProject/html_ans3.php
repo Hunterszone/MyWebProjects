ff5101a864df9a24949ad26cec4d55a3f00cca60
Konstantin Drenski <Konstantin.Drenski@swisscom.com>
2020-05-13 17:21:36 +0300

    css file renamed

--- Apps4Web/WebSiteProject/html_ans3.php
+++ Apps4Web/WebSiteProject/html_ans3.php
@@ -6,7 +6,7 @@
 	
 	<title>HTML Quiz</title>
 	
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 
 <body>
--- Apps4Web/WebSiteProject/javascript_ans.php
+++ Apps4Web/WebSiteProject/javascript_ans.php
@@ -6,7 +6,7 @@
 	
 	<title>PHP Quiz</title>
 	
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 
 <body>
