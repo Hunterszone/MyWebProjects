b524117d5e79826bfc90bcd58d6511fe8342f589
Konstantin Drenski <Konstantin.Drenski@swisscom.com>
2020-05-15 13:21:54 +0300

    code cleanup

--- Apps4Web/WebSiteProject/simple_login.sql
+++
@@ -1,9 +0,0 @@
-
-
-    CREATE TABLE IF NOT EXISTS `users` (
-      `mem_id` int(11) NOT NULL AUTO_INCREMENT,
-      `username` varchar(50) NOT NULL,
-      `password` varchar(50) NOT NULL,
-      PRIMARY KEY (`mem_id`)
-    ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
