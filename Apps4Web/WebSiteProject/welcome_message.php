b01f39135e201bfdb1d1157814b6b58c8ab0ff65
Konstantin Drenski <Konstantin.Drenski@swisscom.com>
2020-05-17 22:06:25 +0300

    more user details extracted

--- Apps4Web/WebSiteProject/welcome_message.php
+++ Apps4Web/WebSiteProject/welcome_message.php
@@ -1,22 +1,46 @@
 <?php           
                 require_once('authentication.php');
                 require_once('member.php');
-                
+				require_once('geoplugin.class.php');
+	
+				$geoplugin = new geoPlugin();
+				$geoplugin->locate();
+				$remoteIp = htmlspecialchars($_SERVER['REMOTE_ADDR']);
+				//$xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=". getUserIpAddr());
+
                 echo "<center>";
                 echo "<span style='font-size:20px'>";
                 echo "<p>Welcome, " . htmlspecialchars($_SESSION['SESS_USERNAME'], ENT_QUOTES, 'UTF-8');
                 echo "!</p>";
-                echo "<p>Your hostname is: " . htmlspecialchars($_SERVER['SERVER_NAME'], ENT_QUOTES, 'UTF-8');
-                echo "</p>";
-                $ip = gethostbyname($_SERVER['SERVER_NAME']);
-                echo "<p>Your IP address is: " . htmlspecialchars($ip, ENT_QUOTES, 'UTF-8');
+				echo "<p>Your personal ID is: " . htmlspecialchars($_SESSION['SESS_MEMBER_ID'], ENT_QUOTES, 'UTF-8');
                 echo "</p>";
                 echo "<p>Your email is: " . htmlspecialchars($_SESSION['SESS_EMAIL'], ENT_QUOTES, 'UTF-8');
                 echo "</p>";
-                echo "<p>Your personal ID is: " . htmlspecialchars($_SESSION['SESS_MEMBER_ID'], ENT_QUOTES, 'UTF-8');
+                echo "<p>Your hostname is: " . htmlspecialchars($_SERVER['SERVER_NAME'], ENT_QUOTES, 'UTF-8');
+                echo "</p>";
+                echo "<p>Your remote IP address is: " . htmlspecialchars($remoteIp, ENT_QUOTES, 'UTF-8');
+                echo "</p>";
+				echo "<p>Your client IP address is: {$geoplugin->ip}";
+                echo "</p>";
+				echo "<p>Country of origin: {$geoplugin->countryName}";
+                echo "</p>";
+				echo "<p>City of origin: {$geoplugin->city}";
                 echo "</p>";
                 echo "</center>";
                 echo "</font>";
+				
+				function getUserIpAddr(){
+					if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+						//ip from share internet
+						$ip = $_SERVER['HTTP_CLIENT_IP'];
+					}elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+						//ip pass from proxy
+						$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+					}else{
+						$ip = $_SERVER['REMOTE_ADDR'];
+					}
+					return $ip;
+				}
                 
 ?>
 <html>
