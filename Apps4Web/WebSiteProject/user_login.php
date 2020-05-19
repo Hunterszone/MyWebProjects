a477a1017c9ef23458a8e51cda16821f313d291d
Konstantin Drenski <Konstantin.Drenski@swisscom.com>
2020-05-19 23:13:46 +0300

    feedback in new tab

--- Apps4Web/WebSiteProject/user_login.php
+++ Apps4Web/WebSiteProject/user_login.php
@@ -759,7 +759,7 @@
             </div>
          </div>
          <button class="nicetoo" onclick="location.href='forgotPass.php'"><i class="fa fa-unlock"></i> New pass</button>
-         <button class="nicetoo" onclick="location.href='contact_form.php'"><i class="fa fa-comments"></i> Contact us</button>
+         <button class="nicetoo" onclick=window.open("contact_form.php")><i class="fa fa-comments"></i> Contact us</button>
          <button class="nicetoo" onclick="location.href='index.php'"><i class="fa fa-sign-out"></i> Logout</button>
       </center>
       </br>  
