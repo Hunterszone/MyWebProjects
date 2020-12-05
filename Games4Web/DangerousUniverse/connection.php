94b603824a59b68196c1b36ad3663200cc12b572
kdrenski <k.drenski91@gmail.com>
2020-12-05 16:23:39 +0200

    fallback in case of missing DB

--- Games4Web/DangerousUniverse/connection.php
+++ Games4Web/DangerousUniverse/connection.php
@@ -12,20 +12,13 @@
     $conn = new mysqli($host, $user, $pass, $db, 3306) or die("Could not connect to the database");
 	mysqli_select_db($conn, $db) or die("Could not select database. Click <a href='".$fallback_address."'>here</a> to return to game.");
 	
+	if(is_null($db)) {
+		echo "Could not select database. Click <a href='".$fallback_address."'>here</a> to return to game.";
+	}
+	
     if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }
-	
-	// ob_start();
-	// echo "This output will be sent to the browser";	
-	
-	/*$sql = "INSERT INTO users (username, password) VALUES ('JohnDoe', 'Today123')";
-
-	if ($conn->query($sql) === TRUE) {
-	  echo "New record created successfully";
-	} else {
-	  echo "Error: " . $sql . "<br>" . $conn->error;
-	}*/
 	
 ?>
 
