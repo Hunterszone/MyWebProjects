6fb46ede911d193f3f6f75d87720efe882377c12
kdrenski <k.drenski91@gmail.com>
2021-04-22 04:50:41 +0300

    refactoring

--- Apps4Web/WebSiteProject/index.php
+++ Apps4Web/WebSiteProject/index.php
@@ -12,39 +12,40 @@
             <div class="dropdown">
                <button class="dropbtn"> Login</button>
                <div class="dropdown-content">
-                  <button type="button" id="but1" onclick="window.location.href='./forms/login_form.php'" value="Password">With Password</button>
-                  <button type="button" id="but1" onclick="window.location.href='./qr/generateQR.php'" value="2FA/QR code">With QR Code</button>
+                  <button type="button" id="but1" style="cursor:pointer" onclick="window.location.href='./forms/login_form.php'" value="Password">With Password</button>
+                  <button type="button" id="but1" style="cursor:pointer" onclick="window.location.href='./qr/generateQR.php'" value="2FA/QR code">With QR Code</button>
                </div>
             </div>
             <div class="dropdown">
                <button class="dropbtn"> Articles</button>
                <div class="dropdown-content">
-                  <button type="button" onclick=window.open("./articles/html.html") value="HTML">HTML</button>
-                  <button type="button" onclick=window.open("./articles/css.html") value="CSS">CSS</button>
-                  <button type="button" onclick=window.open("./articles/javascript.html") value="JS">JS</button>
-                  <button type="button" onclick=window.open("./articles/php.html") value="PHP">PHP</button>
+                  <button type="button" style="cursor:pointer" onclick=window.open("./articles/html.html") value="HTML">HTML</button>
+                  <button type="button" style="cursor:pointer" onclick=window.open("./articles/css.html") value="CSS">CSS</button>
+                  <button type="button" style="cursor:pointer" onclick=window.open("./articles/javascript.html") value="JS">JS</button>
+                  <button type="button" style="cursor:pointer" onclick=window.open("./articles/php.html") value="PHP">PHP</button>
                </div>
             </div>
             <div class="dropdown">
                <button class="dropbtn"> Contact us</button>
                <div class="dropdown-content">
-                  <button type="button" onclick=window.open("./forms/contact_form.php") value="Feedback">Feedback</button>
-                  <button type="button" onclick=window.open("phpBB3/index.php") value="Forum">Forum</button>
+                  <button type="button" style="cursor:pointer" onclick=window.open("./forms/contact_form.php") value="Feedback">Feedback</button>
+                  <button type="button" style="cursor:pointer" onclick=window.open("phpBB3/index.php") value="Forum">Forum</button>
                </div>
             </div> 
          </div>
 
          <div class="header">
-            <h1 class="pulsate">Web Programming World</h1>
-            <h2>Your place for web development and web design</h2>
-            <button onclick="window.location.href='./auth/SignUp.php'")>Sign up</button>
+            <h1 class="pulsate"><a style="color:yellow">Web</a> Programming <a style="color:yellow">World</a></h1>
+            <h2>Your place <a style="color:yellow">for</a> web development <a style="color:yellow">and</a> web design</h2>
+            <button style="cursor:pointer" onclick="window.location.href='./auth/SignUp.php'")>Sign up</button>
          </div>
 
          <div class="search-boxes">
             <!-- HTML for SEARCH BAR -->
             <div id="tfheader">
                <form method="get" action="http://www.google.com/search" target="_blank">
-                  <input type="search" ng-model="name" class="tftextinput" name="q" id="searchbar" placeholder="Google results" size="56" maxlength="120"><input type="submit" value="Search" class="tfbutton" id="searchsubmit" class="searchsubmit" />
+                  <input type="search" ng-model="name" class="tftextinput" name="q" id="searchbar" placeholder="Google results" size="56" maxlength="120">
+				  <input type="submit" value="Search" style="cursor:pointer" class="tfbutton" id="searchsubmit" class="searchsubmit" />
                   <div>
                      <input type="checkbox" class="L1" name="sitesearch" value="bing.com" unchecked /><font size="5"> Bing results</font>
                   </div>
@@ -59,19 +60,19 @@
          </div>
             
          <div class='social-sidebar'>
-               <a class='facebook' target='_blank' title="S H A R E!" onclick="void window.open('http://www.facebook.com/sharer.php?s=100&amp;p[title]=&amp;p[url]=&amp;&p[images][0]=', 'sharer', 'toolbar=0,status=0,width=548,height=325'); return false;" target="_parent" href="javascript: void(0)">
+               <a class='facebook' target='_blank' title="S H A R E !" onclick="void window.open('http://www.facebook.com/sharer.php?s=100&amp;p[title]=&amp;p[url]=&amp;&p[images][0]=', 'sharer', 'toolbar=0,status=0,width=548,height=325'); return false;" target="_parent" href="javascript: void(0)">
                   <span>FACEBOOK</span>
                </a>
                   
-               <a class='twitter' target='_blank' title="S H A R E!" a onclick="void window.open('https://twitter.com/intent/tweet?url=http%3A%2F%2Fwww.example.com%2F&original_referer=.php?s=100&amp;p[title]=&amp;p[url]=&amp;&p[images][0]=', 'sharer', 'toolbar=0,status=0,width=548,height=325'); return false;" target="_parent" href="javascript: void(0)">
+               <a class='twitter' target='_blank' title="S H A R E !" a onclick="void window.open('https://twitter.com/intent/tweet?url=http%3A%2F%2Fwww.example.com%2F&original_referer=.php?s=100&amp;p[title]=&amp;p[url]=&amp;&p[images][0]=', 'sharer', 'toolbar=0,status=0,width=548,height=325'); return false;" target="_parent" href="javascript: void(0)">
                   <span>TWITTER</span>
                </a>
                   
-               <a class='gplus' target='_blank' title="S H A R E!" href="https://plus.google.com/share?url={URL}" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=325,width=548');return false;">
+               <a class='gplus' target='_blank' title="S H A R E !" href="https://plus.google.com/share?url={URL}" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=no,scrollbars=no,height=325,width=548');return false;">
                   <span>GOOGLE+</span>
                </a>
                   
-               <a class='linkedin' target='_blank' title="S H A R E!" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http://YourContentsURLGoesHere.com&amp;title=Article Tile Goes Here&amp;summary=Put your summary here" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=390,width=548');return false;">
+               <a class='linkedin' target='_blank' title="S H A R E !" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http://YourContentsURLGoesHere.com&amp;title=Article Tile Goes Here&amp;summary=Put your summary here" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=no,scrollbars=no,height=390,width=548');return false;">
                   <span>LINKEDIN</span>
                </a>
          </div>
