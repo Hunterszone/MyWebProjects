e12449a87172beb298640c6027029e31e6f70ff7
kdrenski <k.drenski91@gmail.com>
2021-05-01 10:38:55 +0300

    adding bootstrap buttons

--- Apps4Web/WebSiteProject/forms/login_form.php
+++ Apps4Web/WebSiteProject/forms/login_form.php
@@ -3,6 +3,7 @@
    <head>
       <meta charset="UTF-8">
       <title>Login</title>
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    </head>
    <body>
       <?php
@@ -74,7 +75,7 @@
                <td>
                   <div align="right"></div>
                </td>
-               <td><input type="submit" value="Login" /></td>
+               <td><button type="submit" class="btn btn-secondary">Login</button></td>
             </tr>
             <tr>
                <td>
