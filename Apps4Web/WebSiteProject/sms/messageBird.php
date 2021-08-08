49d67a9b885d1c6c4940312616f25093e2de144d
kdrenski <k.drenski91@gmail.com>
2021-08-08 23:07:53 +0300

    replacing API test key

--- Apps4Web/WebSiteProject/sms/messageBird.php
+++ Apps4Web/WebSiteProject/sms/messageBird.php
@@ -3,7 +3,7 @@
 
 		$six_digit_random_number = random_int(100000, 999999);
 		
-		$MessageBird = new \MessageBird\Client('j6YajDRkLWhMe4G2KYKGVyIMB');
+		$MessageBird = new \MessageBird\Client('Lue8VGNDsy87A84DjC8AwSZBE');
 		$Message = new \MessageBird\Objects\Message();
 		$Message->originator = 'TestMessage';
 		$Message->recipients = array('+359889619295');
