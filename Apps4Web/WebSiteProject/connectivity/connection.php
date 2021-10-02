eda040089ef0a64b519fcb07e5d60728cd5bcb67
kdrenski <k.drenski91@gmail.com>
2021-10-02 10:21:14 +0300

    refactoring

--- Apps4Web/WebSiteProject/connectivity/connection.php
+++ Apps4Web/WebSiteProject/connectivity/connection.php
@@ -4,7 +4,6 @@
     $host = 'localhost';
 	$user = 'root';
     $pass = 'root';
-    //$pass = '';
     $db = 'phpwebsite';
 	$prefix = "";
     $conn = new mysqli($host, $user, $pass, $db, 3306) or die("Could not connect to the database");
@@ -13,20 +12,6 @@
     if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }
-	
-	/*$sql = "INSERT INTO users (username, password) VALUES ('JohnDoe', 'Today123')";
 
-	if ($conn->query($sql) === TRUE) {
-	  echo "New record created successfully";
-	} else {
-	  echo "Error: " . $sql . "<br>" . $conn->error;
-	}*/
-	
-	/*alter users table*/
-	/*$sql = "ALTER TABLE users ADD profile_pic varchar(100) NULL AFTER username";
-	if ($conn->query($sql) === TRUE) {
-	}else{
-		die("failed". $conn->connect_error);
-	}*/
 ?>
 
